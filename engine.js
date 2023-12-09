5a79835d1eb87b605935aef8b81e56cee874170f
Kacper Pilarski <xxxxbaobabxxxx@gmail.com>
2023-12-09 22:45:34 +0100

    add js day nigdt

--- engine.js
+++ engine.js
@@ -1,5 +1,14 @@
-{
-    const DayNightButton = () => {
-        
-    }
-}
+const DayNightEffect = () => {
+  const BodyElement = document.querySelector(".js-main");
+
+  BodyElement.classList.toggle("NightMain");
+};
+const DayNightDescription = () => {
+  const DestriptionElement = document.querySelector(".js-description");
+  DestriptionElement.classList.toggle("NightJobDate");
+};
+const init = () => {
+  const DayNightToggle = document.querySelector(".js-buttonToggle");
+  DayNightToggle.addEventListener("click",  DayNightDescription);
+};
+init();
